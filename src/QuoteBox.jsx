3215d819a2485503e86ea4fb5e2996387f8ededf
Joselito Ramirez <tu_empresario0122@hotmail.com>
2022-12-19 20:06:48 -0500

    first commit

--- src/QuoteBox.jsx
+++ src/QuoteBox.jsx
@@ -2,32 +2,37 @@ import React, { useState } from 'react';
 import Quote from '../Quote.json';
 
 const QuoteBox = () => {
-
-    const anotherQuote = Math.floor(Math.random() * Quote.length); //de esta manera recorro todas las citas del json
+const colors = ["#845EC2", "#D65DB1", "#FF6F91","#FF9671","#FFC75F","#F9F871" ] 
+    
+const anotherQuote = Math.floor(Math.random() * Quote.length); //de esta manera recorro todas las citas del json
  
     const [index, setIndex] = useState(0)
 
+    const [color, setColor] = useState(0)
+
+    const randomColorIndex = Math.floor(Math.random() * colors.length)
+
     const changeQuotes = () => {
         setIndex(anotherQuote);
+        setColor(randomColorIndex);
     }
-    const colors = ["#845EC2", "#D65DB1", "#FF6F91","#FF9671","#FFC75F","#F9F871" ] 
-    const randomColorIndex = Math.floor(Math.random() * colors.length)
-    document.body.style = `background: ${colors[randomColorIndex]}`;
+    
+    document.body.style = `background: ${colors[color]}`;
 
       
     return (
         <div className= 'card title'>
-            <h1><i class="fa-solid fa-quote-left"></i>{" "} {Quote[index].quote}{" "}</h1> 
+            <h1 style={{color: `${colors[color]}`}}><i class="fa-solid fa-quote-left"></i>{" "} {Quote[index].quote}
+            {" "}</h1> 
 
-            <h2>Author: {Quote[index].author}{" "}
+            <h2 style={{color: `${colors[color]}`}}>Author: {Quote[index].author}{" "}
             <i class="fa-solid fa-quote-right"></i>
             </h2>
 
             <div className='button'>
-                <h2>Change Quotes</h2>
-                <button onClick={changeQuotes}><i class="fa-solid fa-forward"></i></button>
+             <button onClick={changeQuotes}><i class="fa-solid fa-forward"></i></button>
             </div>
-            
+             
         </div>
     );
 };
